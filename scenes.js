833a89282b8bfce4c515e2916b4914d35129dcf4
Danny Bentley <danny_bentley@hotmail.com>
2025-09-17 13:44:50 +0700

    Update scene image sources

--- scenes.js
+++ scenes.js
@@ -2,49 +2,43 @@ window.SCENES = {
   livdin_v1: {
     group: "Living & Dining",
     title: "View 1",
-    url: "https://i.postimg.cc/gmKG3fyk/VR-copy-done.jpg",
+    url: "assets/images/Living_Room-View1.jpg",
     hotspots: []
   },
   livdin_v2: {
     group: "Living & Dining",
     title: "View 2",
-    url: "https://i.postimg.cc/gmKG3fyk/VR-copy-done.jpg",
+    url: "assets/images/Living_Room-View2.jpg",
     hotspots: []
   },
   livdin_v3: {
     group: "Living & Dining",
     title: "View 3",
-    url: "https://i.postimg.cc/gmKG3fyk/VR-copy-done.jpg",
+    url: "assets/images/Living_Room-View3.jpg",
     hotspots: []
   },
   msbed_v1: {
     group: "Master Bedroom",
     title: "View 1",
-    url: "https://i.postimg.cc/Sqv6kvYR/18-0036-4-BR-VR-MODERN-00-31-done.jpg",
+    url: "assets/images/Master_bedroom-View1.jpg",
     hotspots: []
   },
   msbed_v2: {
     group: "Master Bedroom",
     title: "View 2",
-    url: "https://i.postimg.cc/Sqv6kvYR/18-0036-4-BR-VR-MODERN-00-31-done.jpg",
+    url: "assets/images/Master_bedroom-View2.jpg",
     hotspots: []
   },
   msbed_v3: {
     group: "Master Bedroom",
     title: "View 3",
-    url: "https://i.postimg.cc/SSGd8ZMw/18-0036-4-BR-VR-08-ms-bath-20250825.jpg",
-    hotspots: []
-  },
-  msbed_v4: {
-    group: "Master Bedroom",
-    title: "View 4",
-    url: "https://i.postimg.cc/SSGd8ZMw/18-0036-4-BR-VR-08-ms-bath-20250825.jpg",
+    url: "assets/images/Master_bedroom-View3.jpg",
     hotspots: []
   },
   bedroom2: {
     group: "Bedroom 2",
     title: "View 1",
-    url: "https://i.postimg.cc/xnbt7zTX/18-0036-4-BR-VR-08-bedroom-2-VR-02-xxx.jpg",
+    url: "assets/images/Bedroom2-View1.jpg",
     hotspots: []
   }
 };
